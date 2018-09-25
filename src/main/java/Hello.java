e2143964f95890e19b481454634b0c002960f36f
Tatyana Koval <tkoval@luxoft.com>
2018-09-25 14:04:21 +0600

    Hello added

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,24 @@
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+
+    @Test
+    public void testForTeamCity(){
+        System.setProperty("webdriver.chrome.driver", "src/driver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.drom.ru/");
+
+        WebElement search = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.id("q")));
+            search.clear();
+            search.sendKeys("renault");
+            search.sendKeys(Keys.ENTER);
+            driver.quit();
     }
 }
